05cd431cebc28f8579e76d13c84639cf9a90d544
Yoschoa <yoschoankwankam@gmail.com>
2026-02-19 16:20:30 +0100

    Dummy change

--- assembler/assembler.h
+++ assembler/assembler.h
@@ -1,17 +1,36 @@
-#ifndef
-#define
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
 
-#define LIST_OF_INSTRUCTIONS
+// the maximum number of tokens per istructions
+#define MAX_TOKENS 4
+#define MAX_TOKEN_LENGTH 32;
 
-// Function prototype to fetch the lines of code in he assembly file 
-char fetch();
+typedef enum {
 
-// Function prototype to parse the line of code into hexadecimal
-void parseLine();
+    FORMAT_R,
+    FORMAT_I,
+    FORMAT_B,
+    FORMAT_JI,
+    FORMAT_JR,
 
-// function prototype to run the assembler logic
-void run();
+} FORMAT;
+
+struct INSTRUCTION_FORMAT {
+    char    *mnemonic;
+    int     opCode;
+    FORMAT  format;
+};
 
 
+
+/* Function prototype to fetch a line of code and tokenise it.
+The function returns a 2D array of tokens where each row represents tokens per line in .asm file */  
+int tokenise(char *line, char tokens[MAX_TOKENS][MAX_TOKEN_LENGTH]);
+
+/* Function prototype to parse each row of the 2D token array into a hexadecimal instruction word*/
+int  parseTokens(char tokens[][MAX_TOKEN_LENGTH], int count);
+
+// the function runASM will runs the cycle fetch -> tokenise -> parse / encode -> store in bin file
+void runASM();
 
 #endif
