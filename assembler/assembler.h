fa34ca76afdf4a319f5add9d46fa050b3fd3c566
Yoschoa <yoschoankwankam@gmail.com>
2026-02-19 15:41:48 +0100

    First Commit

---
+++ assembler/assembler.h
@@ -0,0 +1,17 @@
+#ifndef
+#define
+
+#define LIST_OF_INSTRUCTIONS
+
+// Function prototype to fetch the lines of code in he assembly file 
+char fetch();
+
+// Function prototype to parse the line of code into hexadecimal
+void parseLine();
+
+// function prototype to run the assembler logic
+void run();
+
+
+
+#endif
