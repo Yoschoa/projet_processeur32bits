5a8f0aadedf36ae1506c89a052e22632d45a4216
Yoschoa <yoschoankwankam@gmail.com>
2026-02-19 19:09:15 +0100

    Started implementing assembler logic

--- assembler/assembler.h
+++ assembler/assembler.h
@@ -1,17 +1,39 @@
-#ifndef
-#define
+#ifndef ASSEMBLER.H
+#define ASSEMBLER.H
+#include <stdio.h>
 
-#define LIST_OF_INSTRUCTIONS
+#define MAX_NUMBER_OF_INSTRUCTIONS 19
 
-// Function prototype to fetch the lines of code in he assembly file 
-char fetch();
+typedef enum  {
+    FORMAT_R,
+    FORMAT_I,
+    FORMAT_J,
+    FORMAT_JR,
+    FORMAT_JI,
+    FORMAT_B,
+    FORMAT_S,
+    FORMAT_H,
 
-// Function prototype to parse the line of code into hexadecimal
-void parseLine();
+} FORMAT;
 
-// function prototype to run the assembler logic
-void run();
+typedef struct  {
+    int     opcode;
+    char    mnemonic[10];
+    FORMAT  format[2];
+    int     numOfOperands;
+} INSTRUCTION_LAYOUT;
+
+/* Function to validate a line of code in assembly returns 0 if line is valid (in regards to the number of operands needed to execute the instruction called in the line), -1 otherwise*/
+int validateCode(char mnemonic[10], int numOfOperands);
+
+/*Function to get the format of and instruction(I, R, JI, JR, etc.) using it's mnemonic and 2nd operand*/
+FORMAT getInstructionFormat(char mnemonic[10], char operand[10]);
+
+/* Function to parse/encode instruction and operands into a 8 byte hexadecimal word*/
+int encode(int opcode, int srcReg1,int imm, int srcReg2, int srcReg3);
 
 
+/* Function to run the different steps of the assembler logic*/
+void runASM(FILE *fptr);
 
 #endif
